b1e30ca0b8bcb1985f321e38ca234b254f6cdc12
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-28 19:45:52 -0800

    release

--- BikeRestService/Program.cs
+++ BikeRestService/Program.cs
@@ -20,5 +20,6 @@ namespace BikeRestService
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
